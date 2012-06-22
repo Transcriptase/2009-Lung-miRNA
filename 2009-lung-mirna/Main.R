98f86b6e5867f7417f88324eee1fd3a30b5fd79c
Russell Williams <russell.d.williams@gmail.com>
2012-06-22 17:27:16 -0400

    Implemented data read-in, corrected for compact format.

--- 2009-lung-mirna/Main.R
+++ 2009-lung-mirna/Main.R
@@ -1,1 +1,39 @@
-#Script goes here
+#Install Packages
+library("AgiMicroRna")
+
+#Pull the internal read.agiMicroRna function
+SCRIPT_PATH <- "~/GitHub/2009-Lung-miRNA/2009-lung-mirna"
+setwd(SCRIPT_PATH)
+source("read.agiMicroRna.R")
+
+#Read target file
+TARGET_PATH <- "~/GitHub/2009-Lung-miRNA/data/sample_groups.txt"
+targets <- readTargets(TARGET_PATH)
+
+#Read the data files
+DATA_PATH <- "M:/MRSTran/Lab Notebook/Array/Lung 2009/Raw Data/miRNA 12-2009/P.Tran_MicroRNA_012210"
+setwd(DATA_PATH)
+
+#Usual workflow would use readMicroRnaAFE(), which is a wrapper for
+#read.maimages in the limma package, but because the files are in 
+#compact rather than full format, I have to re-wrap the underlying function
+#based on advice from the package author in
+#https://www.stat.math.ethz.ch/pipermail/bioconductor/2010-August/035136.html
+# and http://permalink.gmane.org/gmane.science.biology.informatics.conductor/28127
+# and http://www.mentby.com/Group/bioconductor-list/agimicrorna-problems.html
+
+
+
+dd <- read.agiMicroRna(targets,
+                       columns=list(TGS="gTotalGeneSignal",
+                                    TPS="gTotalProbeSignal",
+                                    #meanS="gMeanSignal",
+                                    procS="gProcessedSignal"),
+                       other.columns=list(IsGeneDetected="gIsGeneDetected",
+                                          IsSaturated="gIsSaturated",
+                                          IsFeatNonUnifOF="gIsFeatNonUnifOL",
+                                          IsFeatPopnOL="gIsFeatPopnOL",
+                                          BGKmd="gBGMedianSignal",
+                                          BGKus="gBGUsed"),
+                       annotation = c( "ControlType", "ProbeName","GeneName"),
+                       verbose=TRUE)
---
+++ 2009-lung-mirna/read.agiMicroRna.R
@@ -0,0 +1,109 @@
+`read.agiMicroRna` <-
+function(targets,columns=NULL,other.columns = NULL,annotation=NULL,
+          exp.names=NULL,verbose=FALSE){
+
+## code adapted from read.maimages function from limma package 
+## it creates the uRNAList class: see AgiMicroRNA-classes.R for 
+## the definition of this class and associated methods  
+
+filess=as.character(targets$FileName)
+
+if (is.null(filess)) {
+            stop("targets frame doesn't contain FileName column")
+}
+
+if (is.null(columns)){
+  	columns=list(TGS="gTotalGeneSignal",
+	             TPS="gTotalProbeSignal",
+	  	     meanS="gMeanSignal",
+		     procS="gProcessedSignal")
+}
+
+if (!is.list(columns)){
+            stop("columns must be a list")
+            }
+ 
+if (is.null(other.columns)){
+other.columns=list(IsGeneDetected="gIsGeneDetected",
+				        IsSaturated="gIsSaturated",
+				        IsFeatNonUnifOF="gIsFeatNonUnifOL",
+				        IsFeatPopnOL="gIsFeatPopnOL",
+				        BGKmd="gBGMedianSignal",
+				        BGKus="gBGUsed")
+} 
+
+if (!is.list(other.columns)){
+            stop("other.columns must be a list")
+            }
+
+if (is.null(exp.names)){
+annotation = c( "ControlType", "ProbeName","GeneName")
+}
+
+if (is.null(exp.names)){
+            exp.names = rownames(targets) 
+}
+
+
+cnames <- names(columns)
+required.col = unique(c(annotation, unlist(columns), unlist(other.columns))) 
+
+
+obj = read.columns(filess[1], required.col, text.to.search="",skip = 9, 
+  sep = "\t",quote="\"", stringsAsFactors = FALSE,flush=TRUE)
+narray = length(filess)
+ngenes = dim(obj)[1]
+
+# ngenes = length(scan(filess[1],skip=10,what="integer",flush=TRUE,quiet=TRUE))
+Y = matrix(NA, nrow = ngenes, ncol = narray)
+colnames(Y) =  exp.names 
+
+# R, B, Rb, Gb 
+Newagi = columns
+for (a in cnames){
+ Newagi[[a]] <- Y
+}
+
+# targets 
+Newagi$targets = data.frame(targets$FileName)
+rownames(Newagi$targets) = exp.names
+colnames(Newagi$targets) = "FileName"
+
+# $genes ("ControlType","ProbeName","GeneName") 
+j <- match(annotation, colnames(obj), 0)
+if (any(j > 0)){
+            Newagi$genes <- data.frame(obj[, j, drop = FALSE], check.names = FALSE)
+}
+# $other 
+other.columns <- as.character(other.columns)
+        j <- match(other.columns, colnames(obj), 0)
+        if (any(j > 0)) {
+            other.columns <- colnames(obj)[j]
+            Newagi$other = list()
+            for (j in other.columns) Newagi$other[[j]] <- Y
+        }
+        
+
+for(n in 1:narray) {
+ if(verbose){
+  cat("reading file ",n," - ",filess[n],"\n")
+  }
+    if(n > 1){
+    obj = read.columns(filess[n], required.col, text.to.search="",skip = 9, 
+        sep = "\t",quote="\"", stringsAsFactors = FALSE,flush=TRUE)
+    }
+      
+    for (a in cnames){
+      Newagi[[a]][, n] <- obj[, columns[[a]]]    # R, G, Rb, Gb
+    }
+    
+    for (j in other.columns) {
+                Newagi$other[[j]][, n] <- obj[, j]
+    }    
+
+} ## for  
+
+  # defined in AgiMicroRNA-classes.R USING classes.R limma FILE 
+  new("uRNAList", Newagi)
+   
+} ## end function 
